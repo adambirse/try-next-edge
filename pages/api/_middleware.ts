3fd36bfd2092c08c3c609158fe86be43b727475a
Adam Birse <adambirse@gmail.com>
2021-11-19 15:37:04 +0000

    Upstash middleware added.

---
+++ pages/api/_middleware.ts
@@ -0,0 +1,10 @@
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import {auth, get} from '@upstash/redis';
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+   const country = req.geo.country || 'GB';
+   auth('UPSTASH_REDIS_REST_URL', 'UPSTASH_REDIS_REST_TOKEN');
+   let result = await get(country);
+   let greeting = result.data || 'Hello World!'
+   return new Response(greeting)
+}
