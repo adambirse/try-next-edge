48acdfbcb4cc8b1d4ff6474db9512599118d6314
Adam Birse <adambirse@gmail.com>
2021-11-19 16:15:51 +0000

    JSON response with country.

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,10 +1,21 @@
-import type { NextFetchEvent, NextRequest } from 'next/server'
-import {auth, get} from '@upstash/redis';
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { auth, get } from "@upstash/redis";
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-   const country = req.geo.country || 'GB';
-   auth(process.env.UPSTASH_REDIS_REST_URL, process.env.UPSTASH_REDIS_REST_TOKEN);
-   let result = await get(country);
-   let greeting = result.data || 'Hello World!'
-   return new Response(greeting)
+  const country = req.geo.country || "GB";
+  auth(
+    process.env.UPSTASH_REDIS_REST_URL,
+    process.env.UPSTASH_REDIS_REST_TOKEN
+  );
+  let result = await get(country);
+  let greeting = result.data || "Hello World!";
+  return new Response(
+    JSON.stringify({ greeting: greeting, country: req.geo.country }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
