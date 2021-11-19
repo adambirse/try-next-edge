9d0745eb5d7517cea08936dcf2cbed83cf72fc3d
adambirse <adambirse@gmail.com>
2021-11-19 18:33:20 +0000

    Change default name

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,5 +10,5 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   const languageSpecifiGreeting = req.query.greeting;
-  res.status(200).json({ greeting: `${languageSpecifiGreeting} John Doe` })
+  res.status(200).json({ greeting: `${languageSpecifiGreeting} Drunk Andy` })
 }
